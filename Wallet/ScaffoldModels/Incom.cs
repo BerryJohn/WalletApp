253a66bb1d92ae94a2f087fc7bec81e2e106811b
Jan Bak <jan.pawel.bak01@gmail.com>
2021-01-25 19:23:21 +0100

    First commit

---
+++ Wallet/ScaffoldModels/Incom.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace NorthwindEFCore.ScaffoldModels
+{
+    [Index(nameof(Id), IsUnique = true)]
+    [Index(nameof(UserId), IsUnique = true)]
+    public partial class Incom
+    {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+        [Column("userId")]
+        public long UserId { get; set; }
+        [Column("income")]
+        public long Income { get; set; }
+        [Column("categoryId")]
+        public long CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        [InverseProperty(nameof(IncomeCategory.Incoms))]
+        public virtual IncomeCategory Category { get; set; }
+        [ForeignKey(nameof(UserId))]
+        [InverseProperty("Incom")]
+        public virtual User User { get; set; }
+    }
+}
---
+++ Wallet/ScaffoldModels/Outgoing.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore;
+
+#nullable disable
+
+namespace NorthwindEFCore.ScaffoldModels
+{
+    [Index(nameof(Id), IsUnique = true)]
+    public partial class Outgoing
+    {
+        [Key]
+        [Column("id")]
+        public long Id { get; set; }
+        [Column("userId")]
+        public long UserId { get; set; }
+        [Column("outcome")]
+        public long Outcome { get; set; }
+        [Column("categoryId")]
+        public long CategoryId { get; set; }
+
+        [ForeignKey(nameof(CategoryId))]
+        [InverseProperty(nameof(OutgoingsCategory.Outgoings))]
+        public virtual OutgoingsCategory Category { get; set; }
+        [ForeignKey(nameof(UserId))]
+        [InverseProperty("Outgoings")]
+        public virtual User User { get; set; }
+    }
+}

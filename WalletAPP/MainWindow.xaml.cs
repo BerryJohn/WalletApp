20da4c260c7400d1e0d15690fcb7a75df52888b4
Jan Bak <jan.pawel.bak01@gmail.com>
2021-02-04 17:05:48 +0100

    Basic user add form

--- WalletAPP/MainWindow.xaml.cs
+++ WalletAPP/MainWindow.xaml.cs
@@ -9,24 +9,38 @@ namespace WalletAPP
 {
     public partial class MainWindow : Window
     {
+        private void UpdateUserList()
+        {
+            lista.Items.Clear();
+            using var db = new Wallet();
+            IQueryable<User> users = db.Users;
+            foreach (var user in users)
+                lista.Items.Add($"{user.Nickname}-{user.Money}PLN");
+        }
         public MainWindow()
         {
             InitializeComponent();
+            UpdateUserList();
+        }
+
+
+
+        private void lista_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            string[] curItem = lista.SelectedItem.ToString().Split('-');
+            currUser.Content = curItem[0];
         }
 
         private void btn_Click(object sender, RoutedEventArgs e)
         {
             using var db = new Wallet();
-            IQueryable<User> users = db.Users;
-            lista.Items.Clear();
-            foreach (var user in users)
-                lista.Items.Add($"{user.Nickname}-{user.Money}PLN");
-        }
-
-        private void lista_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
-        {
-            string curItem = lista.SelectedItem.ToString();
-            tekst.Content = curItem;
+            
+            var newUser = new User();
+            newUser.Nickname = new_user_nick.Text;
+            newUser.Money = 0;
+            db.Users.Add(newUser);
+            int succes = db.SaveChanges();
+            UpdateUserList();
         }
     }
 }

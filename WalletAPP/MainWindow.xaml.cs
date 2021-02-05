97aa491455417c29f60574fb7aa2f504fc12f001
Jan Bak <jan.pawel.bak01@gmail.com>
2021-02-05 18:47:28 +0100

    New page, showing incoms TBA

--- WalletAPP/MainWindow.xaml.cs
+++ WalletAPP/MainWindow.xaml.cs
@@ -42,5 +42,10 @@ namespace WalletAPP
             int succes = db.SaveChanges();
             UpdateUserList();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Login.Content = new Page1();
+        }
     }
 }
---
+++ WalletAPP/Page1.xaml.cs
@@ -0,0 +1,35 @@
+﻿
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using WalletDB.ScaffoldModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace WalletAPP
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class Page1 : Page
+    {
+        private void UpdateUserList()
+        {
+            IncomeList.Items.Clear();
+            using var db = new Wallet();
+            IQueryable<Incom> Incoms = db.Incoms;
+            foreach (var incom in Incoms)
+                IncomeList.Items.Add($"+{incom.Income}PLN  Category:{incom.CategoryId}"); //ID IS TEMPORARY
+        }
+        public Page1()
+        {
+            InitializeComponent();
+            UpdateUserList();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+           
+        }
+    }
+}

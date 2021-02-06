04a50917c4dd48e39f244bc48f1f8b6210fbd239
Jan Bak <jan.pawel.bak01@gmail.com>
2021-02-07 00:48:29 +0100

    Adding income is possible, only for one user

--- WalletAPP/Page1.xaml.cs
+++ WalletAPP/Page1.xaml.cs
@@ -13,23 +13,80 @@ namespace WalletAPP
     /// </summary>
     public partial class Page1 : Page
     {
+        private string findCategory(long id)
+        {
+            using var db = new Wallet();
+            IQueryable<IncomeCategory> category = db.IncomeCategories.Where(el => el.Id == id);
+            return category.First().Category;
+        }
         private void UpdateUserList()
         {
             IncomeList.Items.Clear();
             using var db = new Wallet();
             IQueryable<Incom> Incoms = db.Incoms;
             foreach (var incom in Incoms)
-                IncomeList.Items.Add($"+{incom.Income}PLN  Category:{incom.CategoryId}"); //ID IS TEMPORARY
+                IncomeList.Items.Add($"+{incom.Income}PLN  Category: {findCategory(incom.CategoryId)}"); 
+        }
+        private void UpdateCategoryList()
+        {
+            incomeFormCategories.Items.Clear();
+            using var db = new Wallet();
+            IQueryable<IncomeCategory> categories = db.IncomeCategories;
+            foreach (var category in categories)
+                incomeFormCategories.Items.Add($"{category.Category}"); 
         }
         public Page1()
         {
             InitializeComponent();
             UpdateUserList();
+            UpdateCategoryList();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
            
+        }
+
+        private void incomeFormCategories_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private long findCategoryID(string category)
+        {
+            using var db = new Wallet();
+            IQueryable<IncomeCategory> categories = db.IncomeCategories.Where(el => el.Category == category);
+            return categories.First().Id;
+        }
+        private int addCategory(long amount, string category)
+        {
+            using var db = new Wallet();
+            var newIncom = new Incom();
+            newIncom.Income = amount;
+            newIncom.CategoryId = findCategoryID(category);
+            newIncom.UserId = 1; 
+#warning TODO user selection now working only for first user
+            db.Incoms.Add(newIncom);
+            int succes = db.SaveChanges();
+            UpdateUserList();
+            return succes;
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string amount = incomeFormValue.Text;
+            string category = "";
+            try 
+            {
+                category = incomeFormCategories.SelectedItem.ToString();
+            }
+            catch
+            {
+                return;
+            }
+            if (long.TryParse(amount,out long res) && !string.IsNullOrEmpty(category))
+            {
+                addCategory(res, category);
+            }
         }
     }
 }
